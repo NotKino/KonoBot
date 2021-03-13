afe4fd7a0e746999e21f7a0eb73f4f357d7e600f
Kino <70085049+NotKino@users.noreply.github.com>
2021-03-13 01:28:58 +0000

    Small Change.

--- utils/modles.py
+++ utils/modles.py
@@ -1,14 +1,6 @@
 import discord
 import time
 
-def get_member(self, member_id):
-
-    for guild in self.guilds:
-        member = guild.get_member(member_id)
-        if member:
-            return member
-            
-
 def get_message(self, message_id):
 
     return self._connection._get_message(message_id)
@@ -20,6 +12,5 @@ async def latency(self):
     await self.trigger_typing()  # not sure if /users/@me would be better
     return time.perf_counter() - lat
 
-discord.Client.get_member = get_member
 discord.Client.get_message = get_message
 discord.abc.Messageable.latency = latency
